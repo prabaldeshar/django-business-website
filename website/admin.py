1fcf58fb29efd6d90938ec68782cc178196cb888
Prabal Deshar <prabaldeshar@gmail.com>
2025-01-27 12:25:58 +0545

    add: inline editing for ProjectImage in Project admin

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, ProjectImage
 # Register your models here.
 
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'client_name', 'location']
     list_filter = ['project_type']
     search_fields = ['title', 'description', 'client_name', 'location']
+    inlines =[ProjectImageInline, ]
