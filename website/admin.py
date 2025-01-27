83995ccd1197d022bab4ea871519a62dfa31578c
Prabal Deshar <prabaldeshar@gmail.com>
2025-01-27 11:43:18 +0545

    add: field project type in Project model

--- website/admin.py
+++ website/admin.py
@@ -6,4 +6,5 @@ from .models import Project
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'client_name', 'location']
+    list_filter = ['project_type']
     search_fields = ['title', 'description', 'client_name', 'location']
--- website/models.py
+++ website/models.py
@@ -13,9 +13,18 @@ class BaseModel(models.Model):
         abstract = True
 
 class Project(BaseModel):
+    PROJECT_TYPE_RESIDENTIAL = "RESIDENTIAL"
+    PROJECT_TYPE_COMMERCIAL = "COMMERCIAL"
+
+    project_type_choices = [
+        (PROJECT_TYPE_RESIDENTIAL, ) * 2,
+        (PROJECT_TYPE_COMMERCIAL, ) * 2
+    ]
+
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     client_name = models.CharField(max_length=100, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     cover_image = models.FileField(null=True, blank=True, upload_to='projects/images/', validators=[django.core.validators.FileExtensionValidator(allowed_extensions=['jpeg', 'jpg', 'png', 'pdf'])])
+    project_type = models.CharField(max_length=20, choices=project_type_choices, default=PROJECT_TYPE_COMMERCIAL)
     completed_date = models.DateField(default=date.today, null=True, blank=True)
