2d0f8486e2fe8599cfa4c87b180cef934059ea56
Prabal Deshar <prabaldeshar@gmail.com>
2025-01-27 09:48:49 +0545

    create Project model and s3 configuration

--- website/models.py
+++ website/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import date
+import django.core.validators
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class Project(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    client_name = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    cover_image = field=models.FileField(null=True, upload_to='projects/images/', validators=[django.core.validators.FileExtensionValidator(allowed_extensions=['jpeg', 'jpg', 'png', 'pdf'])]),
+    completed_date = models.DateField(default=date.today, null=True, blank=True)
