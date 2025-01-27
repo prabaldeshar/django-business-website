f1b8c6cd5b1a385fd55e2e69722cec884225bd68
Prabal Deshar <prabaldeshar@gmail.com>
2025-01-27 17:17:57 +0545

    add: exclude deleted fields from Project and ProjectImage admin interfaces

--- website/admin.py
+++ website/admin.py
@@ -4,10 +4,12 @@ from .models import Project, ProjectImage
 
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
+    exclude = ('deleted_at', 'is_deleted')
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'client_name', 'location']
     list_filter = ['project_type']
     search_fields = ['title', 'description', 'client_name', 'location']
+    exclude = ('deleted_at', 'is_deleted')
     inlines =[ProjectImageInline, ]
