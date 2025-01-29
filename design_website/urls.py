8f2a2cf0c9126c2ae80f690db7d090d5e9c38b9b
Prabal Deshar <prabaldeshar@gmail.com>
2025-01-29 21:39:54 +0545

    fix: update root URL to include admin path and route to website URLs

--- design_website/urls.py
+++ design_website/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include("website.urls"))
 ]
 
 if settings.DEBUG:
